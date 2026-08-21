f5f8bfcddd9b6c604596d75357a04424a1f7bf07
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-08-21 19:24:48 +0100

    dont depthtest hud

--- packages/vr-engine/src/hud/HUDSurface.tsx
+++ packages/vr-engine/src/hud/HUDSurface.tsx
@@ -134,7 +134,7 @@ export const HUDSurface = ({
     }, [elements, username, anchor]);
 
     return (
-        <Container width={width} height={height} flexDirection="column" pixelSize={pixel_size} pointerEvents="none">
+        <Container width={width} height={height} flexDirection="column" pixelSize={pixel_size} pointerEvents="none" depthTest={false} depthWrite={false}>
             {VERTICALS.map((vertical) => (
                 <Container key={vertical} flexGrow={1} flexBasis={0} flexDirection="row">
                     {HORIZONTALS.map((horizontal) => (
