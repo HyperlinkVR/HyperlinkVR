e353a58ab9f8037c04b63725164d671651ac9b61
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-08-28 18:01:31 +0100

    get vdn

--- packages/vr-engine/src/render/SSGIPass.tsx
+++ packages/vr-engine/src/render/SSGIPass.tsx
@@ -1,11 +1,21 @@
 import { EffectPass } from "postprocessing";
-import { SSGIEffect } from "realism-effects";
+import { SSGIEffect, VelocityDepthNormalPass } from "realism-effects";
 
 import { usePmndrsPass } from "../render/PmndrsCompat";
+import { useMemo } from "react";
+import { useThree } from "@react-three/fiber";
 
-export const SSGIPass = () => {
+export const SSGIPass = ({ options }: { options?: object }) => {
+    const scene = useThree((s) => s.scene);
+    const camera = useThree((s) => s.camera);
+
+    const vdn = useMemo(() => new VelocityDepthNormalPass(scene, camera), [scene, camera]);
+    usePmndrsPass(() => vdn, [vdn]);
+
     usePmndrsPass(
-        (scene, camera) => new EffectPass(camera, new SSGIEffect(scene, camera))
+        () => new EffectPass(camera as any, new SSGIEffect(scene, camera as any, vdn, options)),
+        [scene, camera, vdn, options]
     );
+
     return null;
 };
